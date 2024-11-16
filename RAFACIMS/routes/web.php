23bb0712a18302eb211e8163d388411f4fd8e0e7
ArchieHill <archiedhill@gmail.com>
2024-11-16 23:22:46 +0000

    Removed login route and added back profile middleware

--- RAFACIMS/routes/web.php
+++ RAFACIMS/routes/web.php
@@ -10,13 +10,10 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-//login route
-Route::get('login', function () {
-    return Inertia::render('Auth/Login', [
-        'canResetPassword' => Route::has('password.request'),
-        'status' => session('status'),
-        'canRegister' => Route::has('register'),
-    ]);//+
-})->middleware('guest')->name('login');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
